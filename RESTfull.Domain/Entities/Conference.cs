2f997109d3d0bfe4248755015f1b1bf5ce4c14d5
доена <69032321+chichxh@users.noreply.github.com>
2025-12-14 12:33:48 +0300

    Allow optional navigation data when creating entities

--- RESTfull.Domain/Entities/Conference.cs
+++ RESTfull.Domain/Entities/Conference.cs
@@ -9,16 +9,16 @@ namespace RESTfull.Domain.Entities
     public class Conference
     {
         public int Id { get; set; }
-        public string Index { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string Index { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
         public DateTime? StartTime { get; set; }
         public DateTime? EndTime { get; set; }
 
         public int? VenueId { get; set; }
         [ForeignKey(nameof(VenueId))]
         [JsonIgnore]
-        public virtual Venue Venue { get; set; }
+        public virtual Venue? Venue { get; set; }
 
         [JsonIgnore]
         public virtual ICollection<Direction> Directions { get; set; } = new List<Direction>();
--- RESTfull.Domain/Entities/Section.cs
+++ RESTfull.Domain/Entities/Section.cs
@@ -6,18 +6,18 @@ namespace RESTfull.Domain.Entities
     public class Section
     {
         public int Id { get; set; }
-        public string Index { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string Index { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
         public DateTime? StartTime { get; set; }
         public DateTime? EndTime { get; set; }
 
         public int DirectionId { get; set; }
         [ForeignKey(nameof(DirectionId))]
-        public virtual Direction Direction { get; set; }
+        public virtual Direction? Direction { get; set; }
 
         public int? VenueId { get; set; }
         [ForeignKey(nameof(VenueId))]
-        public virtual Venue Venue { get; set; }
+        public virtual Venue? Venue { get; set; }
     }
 }
